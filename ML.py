7bc0024237f4178092f9307198e0f9158f88ec2a
kwk18 <34220878+kwk18@users.noreply.github.com>
2021-06-22 17:05:28 +0300

    updated RF, DT

--- ML.py
+++ ML.py
@@ -3,35 +3,30 @@ from collections import Counter, namedtuple
 
 
 class GaussianNaiveBayesClassifier():
-    def __init__(self):
-        pass
     
-    def fit(self, X, y):
-        self.priors_ = np.bincount(y) / len(y)
-        self.n_classes_ = np.max(y) + 1
+    def fit(self, X_train, y_train):
+        self.classes = np.unique(y_train)
+        self.n_classes = len(self.classes)
+        self.prior = np.array(X_train.groupby(y_train).apply(lambda col: len(col)) / len(y_train))
+        self.mean = np.array(X_train.groupby(y_train).apply(np.mean))
+        self.var = np.array(X_train.groupby(y_train).apply(np.var))
 
-        self.means_ = np.array([X[np.where(y == i)].mean(axis=0) for i in range(self.n_classes_)])
-        self.stds_ = np.array([X[np.where(y == i)].std(axis=0) for i in range(self.n_classes_)])
+    def gauss_distribution(self, class_idx, x):
+        mean = self.mean[class_idx]
+        var = self.var[class_idx]
+        return np.exp((-1/2) * ((x-mean)**2) / (2 * var)) / np.sqrt(2 * np.pi * var)
 
-        return self
-
-    def predict_proba(self, X):
-        res = []
-        for i in range(len(X)):
-            probas = []
-            for j in range(self.n_classes_):
-                probas.append((1 / np.sqrt(2 * np.pi * self.stds_[j] ** 2) * np.exp(
-                    -0.5 * ((X[i] - self.means_[j]) / self.stds_[j]) ** 2)).prod() * self.priors_[j])
-            probas = np.array(probas)
-            res.append(probas / probas.sum())
-
-        return np.array(res)
-
-    def predict(self, X):
-        res = self.predict_proba(X)
-
-        return res.argmax(axis=1)
-
+    def predict(self, X_test):
+        y_pred = []
+        for x in np.array(X_test):
+            posteriors = []
+            for class_idx in range(self.n_classes):
+                prior = np.log(self.prior[class_idx])
+                conditional = np.sum(np.log(self.gauss_distribution(class_idx, x)))
+                posterior = prior + conditional
+                posteriors.append(posterior)
+            y_pred.append(self.classes[np.argmax(posteriors)])
+        return y_pred
 
 class LR():
     def __init__(self, lr=0.01, steps=5000):
@@ -90,3 +85,113 @@ class Node():
         self.threshold = 0
         self.left = None
         self.right = None
+
+        
+class DTC():
+
+    def __init__(self, max_depth=1, rf=False):
+        self.max_depth = max_depth
+        self.rf = rf
+
+    def fit(self, X, y, max_features=None):
+        self.n_classes_ = len(set(y))
+        if not self.rf:
+            n_features_ = X.shape[1]
+        else:
+            ind = np.random.choice(X.shape[0], X.shape[0])
+            X, y = X[tuple([ind])], y[tuple([ind])]
+            if max_features is None:
+                n_features_ = np.sqrt(X.shape[1]).astype(int)
+            else:
+                n_features_ = max_features
+        self.features_ = np.sort(np.random.choice(X.shape[1], n_features_,
+                                                  replace=False))
+        self.tree_ = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return [self._predict(inputs) for inputs in X]
+
+    def _best_split(self, X, y):
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes_)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in self.features_:
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+            num_left = [0] * self.n_classes_
+            num_right = num_parent.copy()
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[c] += 1
+                num_right[c] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes_)
+                )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes_)
+                )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [np.sum(y == i)
+                                 for i in range(self.n_classes_)]
+        predicted_class = np.argmax(num_samples_per_class)
+        node = Node(predicted_class=predicted_class)
+        if depth < self.max_depth:
+            idx, thr = self._best_split(X, y)
+            if idx is not None:
+                indices_left = X[:, idx] < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature_index = idx
+                node.threshold = thr
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _predict(self, inputs):
+        node = self.tree_
+        while node.left:
+            if inputs[node.feature_index] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.predicted_class
+
+
+
+class RFC():
+    def __init__(self, max_depth=5, n_trees=10):
+        self.max_depth = max_depth
+        self.n_trees = n_trees
+        self.trees = [None] * n_trees
+
+    def fit(self, X, y):
+        for i in range(self.n_trees):
+            
+            idx = np.random.choice(X.shape[0], X.shape[0])
+            X, y = X[tuple([idx])], y[tuple([idx])] # bootstrap
+            
+            n_features = np.sqrt(X.shape[1]).astype(int)
+            X = X[:, np.random.choice(X.shape[1], n_features, replace=False)] #crf
+            
+            self.trees[i] = DTC(self.max_depth)
+            self.trees[i].fit(X, y)
+
+    def predict(self, X):
+        raw_preds = np.zeros((self.n_trees, X.shape[0]))
+        ans = np.empty(X.shape[0])
+        for i in range(self.n_trees):
+            raw_preds[i] = self.trees[i].predict(X)
+        for i in range(len(ans)):
+            ans[i] = Counter(raw_preds[:, i]).most_common(1)[0][0]
+        return ans.astype(int)
